05741e80d5f773c06f4433d6d572e3528e59b3e5
tuyucheng <tuyucheng2000@163.com>
2022-09-27 13:01:30 +0800

    Spring Security Custom Logout Handler

---
+++ spring-security-modules/spring-security-web-boot-2/src/main/java/cn/tuyucheng/taketoday/customlogouthandler/user/User.java
@@ -0,0 +1,26 @@
+package cn.tuyucheng.taketoday.customlogouthandler.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String login;
+
+    private String password;
+
+    private String role;
+
+    private String language;
+}
