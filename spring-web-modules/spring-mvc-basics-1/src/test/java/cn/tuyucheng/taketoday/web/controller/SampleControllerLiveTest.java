6a1d9a304c15601525caa8324be884163695a441
tuyucheng <tuyucheng2000@163.com>
2022-10-04 09:29:19 +0800

    Spring MVC Tutorial
    Spring MVC Tutorial
    
    Spring MVC Tutorial

---
+++ spring-web-modules/spring-mvc-basics-1/src/test/java/cn/tuyucheng/taketoday/web/controller/SampleControllerLiveTest.java
@@ -0,0 +1,31 @@
+package cn.tuyucheng.taketoday.web.controller;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+class SampleControllerLiveTest {
+
+    private static final String SERVICE_BASE_URL = "/spring-mvc-basics";
+
+    @Test
+    void whenSampleEndpointCalled_thenOkResponseObtained() {
+        RestAssured.get(SERVICE_BASE_URL + "/sample")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void whenSample2EndpointCalled_thenOkResponseObtained() {
+        RestAssured.get(SERVICE_BASE_URL + "/sample2")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void whenSample3EndpointCalled_thenOkResponseObtained() {
+        RestAssured.get(SERVICE_BASE_URL + "/sample3")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
