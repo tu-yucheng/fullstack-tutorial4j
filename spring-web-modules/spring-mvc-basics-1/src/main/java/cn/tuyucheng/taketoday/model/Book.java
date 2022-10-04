ae814c72d5e9d45b4d81cf7ff7a1f0dda4ed418b
tuyucheng <tuyucheng2000@163.com>
2022-10-04 10:57:35 +0800

    The Spring @Controller and @RestController Annotations

---
+++ spring-web-modules/spring-mvc-basics-1/src/main/java/cn/tuyucheng/taketoday/model/Book.java
@@ -0,0 +1,14 @@
+package cn.tuyucheng.taketoday.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    private int id;
+    private String author;
+    private String title;
+}
---
+++ spring-web-modules/spring-mvc-basics-1/src/main/java/cn/tuyucheng/taketoday/web/controller/SimpleBookController.java
@@ -0,0 +1,29 @@
+package cn.tuyucheng.taketoday.web.controller;
+
+import cn.tuyucheng.taketoday.model.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("books")
+public class SimpleBookController {
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody Book getBook(@PathVariable int id) {
+        return findBookById(id);
+    }
+
+    private Book findBookById(int id) {
+        Book book = null;
+        if (id == 42) {
+            book = new Book();
+            book.setId(id);
+            book.setAuthor("Douglas Adamas");
+            book.setTitle("Hitchhiker's guide to the galaxy");
+        }
+        return book;
+    }
+}
