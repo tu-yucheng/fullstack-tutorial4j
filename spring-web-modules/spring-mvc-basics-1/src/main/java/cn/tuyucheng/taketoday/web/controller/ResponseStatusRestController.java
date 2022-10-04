fed6143f8cca1f0a1a4161cabfa9701124076e10
tuyucheng <tuyucheng2000@163.com>
2022-10-04 17:21:15 +0800

    Spring @RequestMapping New Shortcut Annotations

---
+++ spring-web-modules/spring-mvc-basics-1/src/main/java/cn/tuyucheng/taketoday/web/controller/ResponseStatusRestController.java
@@ -0,0 +1,43 @@
+package cn.tuyucheng.taketoday.web.controller;
+
+import cn.tuyucheng.taketoday.model.Book;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+public class ResponseStatusRestController {
+
+    @GetMapping("/teapot")
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    public void teaPot() {
+    }
+
+    @GetMapping("empty")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void emptyResponse() {
+    }
+
+    @GetMapping("empty-no-responsestatus")
+    public void emptyResponseWithoutResponseStatus() {
+    }
+
+    @PostMapping("create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book createEntity() {
+        // here we would create and persist an entity
+        int randomInt = ThreadLocalRandom.current().nextInt(1, 100);
+        return new Book(randomInt, "author" + randomInt, "title" + randomInt);
+    }
+
+    @PostMapping("create-no-responsestatus")
+    public Book createEntityWithoutResponseStatus() {
+        // here we would create and persist an entity
+        int randomInt = ThreadLocalRandom.current().nextInt(1, 100);
+        return new Book(randomInt, "author" + randomInt, "title" + randomInt);
+    }
+}
---
+++ spring-web-modules/spring-mvc-basics-1/src/test/java/cn/tuyucheng/taketoday/web/controller/RequestMappingShortcutsIntegrationTest.java
@@ -0,0 +1,82 @@
+package cn.tuyucheng.taketoday.web.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class RequestMappingShortcutsIntegrationTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new RequestMappingShortcutsController()).build();
+    }
+
+    @Test
+    void givenUrl_whenGetRequest_thenFindGetResponse() throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/get");
+
+        ResultMatcher contentMatcher = MockMvcResultMatchers.content()
+                .string("GET Response");
+
+        this.mockMvc.perform(builder)
+                .andExpect(contentMatcher)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenUrl_whenPostRequest_thenFindPostResponse() throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/post");
+
+        ResultMatcher contentMatcher = MockMvcResultMatchers.content()
+                .string("POST Response");
+
+        this.mockMvc.perform(builder)
+                .andExpect(contentMatcher)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenUrl_whenPutRequest_thenFindPutResponse() throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.put("/put");
+
+        ResultMatcher contentMatcher = MockMvcResultMatchers.content()
+                .string("PUT Response");
+
+        this.mockMvc.perform(builder)
+                .andExpect(contentMatcher)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenUrl_whenDeleteRequest_thenFindDeleteResponse() throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.delete("/delete");
+
+        ResultMatcher contentMatcher = MockMvcResultMatchers.content()
+                .string("DELETE Response");
+
+        this.mockMvc.perform(builder)
+                .andExpect(contentMatcher)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void givenUrl_whenPatchRequest_thenFindPatchResponse() throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.patch("/patch");
+
+        ResultMatcher contentMatcher = MockMvcResultMatchers.content()
+                .string("PATCH Response");
+
+        this.mockMvc.perform(builder)
+                .andExpect(contentMatcher)
+                .andExpect(status().isOk());
+    }
+}
