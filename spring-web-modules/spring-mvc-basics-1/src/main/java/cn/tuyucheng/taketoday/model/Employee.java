3023cf884118cc6920338ce3cd5f16ed4c7f3890
tuyucheng <tuyucheng2000@163.com>
2022-10-04 16:57:07 +0800

    Spring MVC Content Negotiation

---
+++ spring-web-modules/spring-mvc-basics-1/src/main/java/cn/tuyucheng/taketoday/model/Employee.java
@@ -0,0 +1,18 @@
+package cn.tuyucheng.taketoday.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlRootElement
+public class Employee {
+    private long id;
+    private String name;
+    private String contactNumber;
+    private String workingArea;
+}
