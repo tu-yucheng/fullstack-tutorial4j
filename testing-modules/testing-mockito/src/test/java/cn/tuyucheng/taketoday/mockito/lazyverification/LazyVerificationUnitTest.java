5705bb56496951a637dbe9dd043df893be6afcba
tuyucheng <tuyucheng2000@163.com>
2022-10-06 16:14:49 +0800

    Lazy Verification with Mockito 2

---
+++ testing-modules/testing-mockito/src/test/java/cn/tuyucheng/taketoday/mockito/lazyverification/LazyVerificationUnitTest.java
@@ -0,0 +1,31 @@
+package cn.tuyucheng.taketoday.mockito.lazyverification;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.exceptions.base.MockitoAssertionError;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.VerificationCollector;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+class LazyVerificationUnitTest {
+
+    @Test
+    void whenLazilyVerified_thenReportsMultipleFailures() {
+        VerificationCollector collector = MockitoJUnit.collector().assertLazily();
+
+        List mockList = mock(List.class);
+        verify(mockList).add("one");
+        verify(mockList).clear();
+
+        try {
+            collector.collectAndReport();
+        } catch (MockitoAssertionError error) {
+            assertTrue(error.getMessage().contains("1. Wanted but not invoked:"));
+            assertTrue(error.getMessage().contains("2. Wanted but not invoked:"));
+        }
+    }
+}
