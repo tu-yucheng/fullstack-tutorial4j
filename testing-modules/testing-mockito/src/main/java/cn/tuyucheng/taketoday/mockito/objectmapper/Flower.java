cabca2a44daddc96be53d6c9e8dfc46af2db39ef
tuyucheng <tuyucheng2000@163.com>
2022-10-09 13:17:09 +0800

    Mocking the ObjectMapper readValue() Method

---
+++ testing-modules/testing-mockito/src/main/java/cn/tuyucheng/taketoday/mockito/objectmapper/Flower.java
@@ -0,0 +1,14 @@
+package cn.tuyucheng.taketoday.mockito.objectmapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Flower {
+
+    private String name;
+    private Integer petals;
+}
