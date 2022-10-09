05d1d19491c8b9e019f9786630a6a3683b51a24c
tuyucheng <tuyucheng2000@163.com>
2022-10-09 15:39:07 +0800

    Quick Guide to BDDMockito

---
+++ testing-modules/testing-mockito/src/test/java/cn/tuyucheng/taketoday/mockito/bddmockito/PhoneBookRepository.java
@@ -0,0 +1,10 @@
+package cn.tuyucheng.taketoday.mockito.bddmockito;
+
+public interface PhoneBookRepository {
+
+    void insert(String name, String phone);
+
+    String getPhoneNumberByContactName(String name);
+
+    boolean contains(String name);
+}
