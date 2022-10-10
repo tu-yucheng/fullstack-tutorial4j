612aa9fd0eca378751f8befb1270dc41f47ef978
tuyucheng <tuyucheng2000@163.com>
2022-10-10 23:15:02 +0800

    refactor code

--- spring-framework/spring-core-1/src/main/java/cn/tuyucheng/taketoday/aware/AwareExample.java
+++ spring-framework/spring-core-1/src/main/java/cn/tuyucheng/taketoday/aware/AwareExample.java
@@ -1,13 +1,17 @@
 package cn.tuyucheng.taketoday.aware;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AwareExample {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        MyBeanName myBeanName = context.getBean(MyBeanName.class);
-        MyBeanFactory myBeanFactory = context.getBean(MyBeanFactory.class);
-        myBeanFactory.getMyBeanName();
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class)) {
+            MyBeanName myBeanName = context.getBean(MyBeanName.class);
+            MyBeanFactory myBeanFactory = context.getBean(MyBeanFactory.class);
+            myBeanFactory.getMyBeanName();
+        } catch (BeansException e) {
+            e.printStackTrace();
+        }
     }
 }
