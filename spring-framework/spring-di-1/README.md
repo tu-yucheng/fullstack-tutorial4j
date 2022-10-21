45ad10325d3fd5cbb176686d2937ae54406c4b8e
tuyucheng <tuyucheng2000@163.com>
2022-10-21 20:09:18 +0800

    Spring Autowiring of Generic Types

--- spring-framework/spring-di-1/README.md
+++ spring-framework/spring-di-1/README.md
@@ -5,7 +5,7 @@ This module contains articles about dependency injection with Spring
 ## Relevant Articles
 
 - [The Spring @Qualifier Annotation](docs/Spring_@Qualifier.md)
-- [Spring Autowiring of Generic Types]()
+- [Spring Autowiring of Generic Types](docs/Spring_Autowire_Generics.md)
 - [Guice vs Spring – Dependency Injection]()
 - [Injecting Prototype Beans into a Singleton Instance in Spring]()
 - [Controlling Bean Creation Order with @DependsOn Annotation]()
---
+++ spring-framework/spring-di-1/src/test/java/cn/tuyucheng/taketoday/dependencyinjectiontypes/DependencyInjectionIntegrationTest.java
@@ -0,0 +1,32 @@
+package cn.tuyucheng.taketoday.dependencyinjectiontypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DependencyInjectionIntegrationTest {
+
+    @Test
+    void givenAutowiredAnnotation_WhenSetOnSetter_ThenDependencyValid() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("dependencyinjectiontypes-context.xml");
+        ArticleWithSetterInjection article = (ArticleWithSetterInjection) context.getBean("articleWithSetterInjectionBean");
+
+        String originalText = "This is a text !";
+        String formattedArticle = article.format(originalText);
+
+        assertEquals(originalText.toUpperCase(), formattedArticle);
+    }
+
+    @Test
+    void givenAutowiredAnnotation_WhenSetOnConstructor_ThenDependencyValid() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("dependencyinjectiontypes-context.xml");
+        ArticleWithConstructorInjection article = (ArticleWithConstructorInjection) context.getBean("articleWithConstructorInjectionBean");
+
+        String originalText = "This is a text !";
+        String formattedArticle = article.format(originalText);
+
+        assertEquals(originalText.toUpperCase(), formattedArticle);
+    }
+}
