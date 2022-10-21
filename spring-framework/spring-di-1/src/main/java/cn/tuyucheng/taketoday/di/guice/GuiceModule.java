df07f49b88fabf5926798fb64b934ae8c822d476
tuyucheng <tuyucheng2000@163.com>
2022-10-21 22:25:19 +0800

    logback config file

---
+++ spring-framework/spring-di-1/src/main/java/cn/tuyucheng/taketoday/di/guice/GuiceModule.java
@@ -0,0 +1,26 @@
+package cn.tuyucheng.taketoday.di.guice;
+
+import cn.tuyucheng.taketoday.di.spring.*;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import lombok.SneakyThrows;
+
+public class GuiceModule extends AbstractModule {
+
+    @SneakyThrows
+    @Override
+    protected void configure() {
+        bind(AccountService.class).to(AccountServiceImpl.class);
+        bind(PersonDao.class).to(PersonDaoImpl.class);
+
+        bind(Person.class).toConstructor(Person.class.getConstructor());
+        bind(FooProcessor.class).toProvider(FooProcessor::new);
+        // bind(Person.class).toProvider(Person::new);
+        bind(Foo.class).toProvider(() -> null);
+    }
+
+    @Provides
+    public BookService bookServiceGenerator() {
+        return new BookServiceImpl();
+    }
+}
---
+++ spring-framework/spring-di-1/src/main/java/cn/tuyucheng/taketoday/di/guice/Person.java
@@ -0,0 +1,5 @@
+package cn.tuyucheng.taketoday.di.guice;
+
+public class Person {
+
+}
