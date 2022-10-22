76f008302ac2b6817b142330fc3d5ecd14d4fce6
tuyucheng <tuyucheng2000@163.com>
2022-10-22 21:20:29 +0800

    Introduction to Pointcut Expressions in Spring

---
+++ spring-framework/spring-aop-1/src/test/java/cn/tuyucheng/taketoday/pointcutadvice/AopLoggingIntegrationTest.java
@@ -0,0 +1,78 @@
+package cn.tuyucheng.taketoday.pointcutadvice;
+
+import cn.tuyucheng.taketoday.Application;
+import cn.tuyucheng.taketoday.pointcutadvice.dao.FooDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {Application.class}, loader = AnnotationConfigContextLoader.class)
+class AopLoggingIntegrationTest {
+
+    @Autowired
+    private FooDao dao;
+    private List<String> messages;
+
+    @BeforeEach
+    void setUp() {
+        messages = new ArrayList<>();
+
+        Handler logEventHandler = new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                messages.add(record.getMessage());
+            }
+
+            @Override
+            public void flush() {
+            }
+
+            @Override
+            public void close() throws SecurityException {
+            }
+        };
+
+        Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+        logger.addHandler(logEventHandler);
+    }
+
+    @Test
+    void givenLoggingAspect_whenCallDaoMethod_thenBeforeAdviceIsCalled() {
+        dao.findById(1L);
+        assertThat(messages, hasSize(1));
+
+        String logMessage = messages.get(0);
+        Pattern pattern = Pattern.compile("^\\[\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}:\\d{3}]findById$");
+        assertTrue(pattern.matcher(logMessage).matches());
+    }
+
+    @Test
+    void givenLoggingAspect_whenCallLoggableAnnotatedMethod_thenMethodIsLogged() {
+        dao.create(42L, "baz");
+        assertThat(messages, hasItem("Executing method: create"));
+    }
+
+    @Test
+    void givenLoggingAspect_whenCallMethodAcceptingAnnotatedArgument_thenArgumentIsLogged() {
+        Foo foo = new Foo(42L, "baz");
+        dao.merge(foo);
+        assertThat(messages, hasItem("Accepting beans with @Entity annotation: " + foo));
+    }
+}
---
+++ spring-framework/spring-aop-1/src/test/java/cn/tuyucheng/taketoday/pointcutadvice/AopPublishingIntegrationTest.java
@@ -0,0 +1,63 @@
+package cn.tuyucheng.taketoday.pointcutadvice;
+
+import cn.tuyucheng.taketoday.Application;
+import cn.tuyucheng.taketoday.pointcutadvice.dao.FooDao;
+import cn.tuyucheng.taketoday.pointcutadvice.events.FooCreationEventListener;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {Application.class}, loader = AnnotationConfigContextLoader.class)
+class AopPublishingIntegrationTest {
+
+    @Autowired
+    private FooDao dao;
+    private Handler logEventHandler;
+    private List<String> messages;
+
+    @BeforeEach
+    void setUp() {
+        logEventHandler = new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                messages.add(record.getMessage());
+            }
+
+            @Override
+            public void flush() {
+            }
+
+            @Override
+            public void close() throws SecurityException {
+            }
+        };
+
+        messages = new ArrayList<>();
+    }
+
+    @Test
+    void givenPublishingAspect_whenCallCreate_thenCreationEventIsPublished() {
+        Logger logger = Logger.getLogger(FooCreationEventListener.class.getName());
+        logger.addHandler(logEventHandler);
+
+        dao.create(1L, "Bar");
+
+        String logMessage = messages.get(0);
+        Pattern pattern = Pattern.compile("Created foo instance: " + Pattern.quote(new Foo(1L, "Bar").toString()));
+        assertTrue(pattern.matcher(logMessage).matches());
+    }
+}
