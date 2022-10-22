e52eb8b9bcffafad4ffa0931ed564684fb0aaf7a
tuyucheng <tuyucheng2000@163.com>
2022-10-22 13:36:07 +0800

    Injecting Spring Beans into Unmanaged Objects

---
+++ spring-framework/spring-di-2/README.md
@@ -0,0 +1,16 @@
+## Spring Dependency Injection - 2
+
+This module contains articles about dependency injection with Spring
+
+## Relevant Articles
+
+- [Injecting Spring Beans into Unmanaged Objects](docs/Spring_InjectBean_IntoUnmanagedObject.md)
+- [Injecting a Value in a Static Field in Spring]()
+- [Spring – Injecting Collections]()
+- [Wiring in Spring: @Autowired, @Resource and @Inject]()
+- [Injecting Spring Beans into Unmanaged Objects]()
+- [Constructor Dependency Injection in Spring]()
+- [Circular Dependencies in Spring]()
+
+- More articles: [[<-- prev]](../spring-di-1/README.md)
+- More articles: [[next -->]](../spring-di-3/README.md)
---
+++ spring-framework/spring-di-2/src/test/java/cn/tuyucheng/taketoday/di/aspectj/PersonUnitTest.java
@@ -0,0 +1,26 @@
+package cn.tuyucheng.taketoday.di.aspectj;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AspectJConfig.class)
+class PersonUnitTest {
+
+    @Test
+    void givenUnmanagedObjects_whenInjectingIdService_thenIdValueIsCorrectlySet() {
+        PersonObject personObject = new PersonObject("tuyucheng");
+        personObject.generateId();
+
+        assertEquals(1, personObject.getId());
+        assertEquals("tuyucheng", personObject.getName());
+
+        PersonEntity personEntity = new PersonEntity("tuyucheng");
+        assertEquals(2, personEntity.getId());
+        assertEquals("tuyucheng", personEntity.getName());
+    }
+}
