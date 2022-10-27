8dc6926d6a012413efa9e93664a91d57fb04a332
tuyucheng <tuyucheng2000@163.com>
2022-10-27 21:44:59 +0800

    Custom Validation MessageSource in Spring Boot

---
+++ spring-boot-modules/spring-boot-mvc-1/src/main/java/cn/tuyucheng/taketoday/springbootmvc/model/LoginForm.java
@@ -0,0 +1,20 @@
+package cn.tuyucheng.taketoday.springbootmvc.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+public class LoginForm {
+
+    @NotEmpty(message = "{email.notempty}")
+    @Email
+    private String email;
+
+    @NotNull
+    private String password;
+}
