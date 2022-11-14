1df095cabd5d44576df8d3fd626c340f26d96783
tuyucheng <tuyucheng2000@163.com>
2022-11-14 12:34:22 +0800

    :sparkles: SpringSecurity5中的新密码存储

--- spring-security-modules/spring-security-web-rest-basic-auth/README.md
+++ spring-security-modules/spring-security-web-rest-basic-auth/README.md
@@ -7,7 +7,7 @@
 - [使用RestTemplate进行基本身份验证](docs/使用RestTemplate进行基本身份验证.md)
 - [SpringSecurity过滤器链中的自定义过滤器](docs/SpringSecurity过滤器链中的自定义过滤器.md)
 - [SpringSecurity基本身份认证](docs/SpringSecurity基本认证.md)
-- []()
+- [SpringSecurity5中的新密码存储](docs/SpringSecurity5中的新密码存储.md)
 - []()
 - []()
 
---
+++ spring-security-modules/spring-security-web-rest-basic-auth/src/main/java/cn/tuyucheng/taketoday/passwordstorage/PasswordStorageApplication.java
@@ -0,0 +1,12 @@
+package cn.tuyucheng.taketoday.passwordstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordStorageApplication.class, args);
+	}
+}
