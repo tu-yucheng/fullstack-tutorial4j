f198f2188cb201e8376cca66b77409f36a38fc01
tuyucheng <tuyucheng2000@163.com>
2022-11-14 11:56:24 +0800

    :sparkles: 如何使用RestTemplate进行基本身份验证

---
+++ spring-security-modules/spring-security-web-rest-basic-auth/README.md
@@ -0,0 +1,15 @@
+## Spring Security Web - REST Basic Authentication
+
+本模块包含关于使用Spring Security在RESTful API中进行基本身份验证的文章
+
+## 相关文章
+
+- [使用RestTemplate进行基本身份验证](docs/使用RestTemplate进行基本身份验证.md)
+- []()
+- []()
+- []()
+- []()
+- []()
+
+- 更多文章： [[<-- prev]]()
+- 更多文章： [[next -->]]()
---
+++ spring-security-modules/spring-security-web-rest-basic-auth/src/main/java/cn/tuyucheng/taketoday/client/spring/ClientConfig.java
@@ -0,0 +1,13 @@
+package cn.tuyucheng.taketoday.client.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("cn.tuyucheng.taketoday.client")
+public class ClientConfig {
+
+	public ClientConfig() {
+		super();
+	}
+}
