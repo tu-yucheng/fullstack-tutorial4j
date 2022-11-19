1327c41a2d64bfecad00928b69450a2f88a27883
tuyucheng <tuyucheng2000@163.com>
2022-11-19 22:10:16 +0800

    :recycle: mockito版本改为”3.3.0“ & AbstractPrivateMethodsUnitTest测试通过

--- software-test/junit-5/src/test/java/cn/tuyucheng/taketoday/param/PersonValidator.java
+++ software-test/junit-5/src/test/java/cn/tuyucheng/taketoday/param/PersonValidator.java
@@ -1,6 +1,5 @@
 package cn.tuyucheng.taketoday.param;
 
-import java.io.Serial;
 import java.util.Arrays;
 
 /**
@@ -18,7 +17,6 @@ public class PersonValidator {
 	 */
 	public static class ValidationException extends Exception {
 
-		@Serial
 		private static final long serialVersionUID = -134518049431883102L;
 
 		// Probably should implement some more constructors, but don't want to tarnish the lesson...
